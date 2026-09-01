919c7f56ba1f12464faf2ef2ff0d7d66d748d3de
Peter Rangga <peterranggasit@gmail.com>
2026-09-01 12:01:17 +0700

    Menambahkan materi pertemuan 3

---
+++ Pertemuan-3-Operator-Struktur-Kendali/Praktik/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        txtNilai = New TextBox()
+        PictureBox1 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(357, 289)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 0
+        Button1.Text = "Input"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' txtNilai
+        ' 
+        txtNilai.Location = New Point(340, 245)
+        txtNilai.Name = "txtNilai"
+        txtNilai.Size = New Size(125, 27)
+        txtNilai.TabIndex = 1
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Location = New Point(340, 115)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(125, 124)
+        PictureBox1.TabIndex = 2
+        PictureBox1.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(PictureBox1)
+        Controls.Add(txtNilai)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtNilai As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+
+End Class
---
+++ Pertemuan-3-Operator-Struktur-Kendali/Praktik/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim nilaiUjian As Integer
+        If Not Integer.TryParse(txtNilai.Text, nilai) Then
+            MessageBox.Show("Masukkan dalam bentuk angka")
+            txtNilai.Focus()
+            Return
+        End If
+
+        If nilaiUjian < 0 OrElse nilaiUjian > 100 Then
+            MessageBox.Show("Masukkan Nilai 0 - 100")
+            tctNilai.Focus()
+            Return
+        End If
+    End Sub
+
+    Private Sub txtNilai_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNilai.KeyPress
+        If Not Char.IsControl(e.KeyChar) AndAlso Not Char.IsDigit(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+End Class
