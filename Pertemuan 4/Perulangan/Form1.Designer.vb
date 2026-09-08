f574784365532b185a7d593c55efa4d185cf344b
Peter Rangga Situmorang <peterranggasit@gmail.com>
2026-09-08 22:03:56 +0700

    pertemuan 4_perulangan

---
+++ Pertemuan 4/Perulangan/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        ListBox1 = New ListBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(188, 97)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(77, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Nilai Awal"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(188, 152)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(78, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Nilai Akhir"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(207, 120)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(207, 184)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 3
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(207, 231)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 4
+        Button1.Text = "Input"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.Location = New Point(377, 97)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(150, 164)
+        ListBox1.TabIndex = 5
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(ListBox1)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Perulangan"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+
+End Class
---
+++ Pertemuan 4/Perulangan/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Clear()
+
+        Dim awal As Integer
+        Dim akhir As Integer
+
+        If Integer.TryParse(TextBox1.Text, awal) AndAlso Integer.TryParse(TextBox2.Text, akhir) Then
+            If awal <= akhir Then
+                For i As Integer = awal To akhir
+                    ListBox1.Items.Add(i)
+                Next
+            Else
+                For i As Integer = awal To akhir Step -1
+                    ListBox1.Items.Add(i)
+                Next
+            End If
+        Else
+            MessageBox.Show("Masukkan angka yang valid!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+
+End Class
